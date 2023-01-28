194bb616f8b6f5f3c76e66c7772769aeafbf79e8
Jitulteron7 <jitulteron9@gmail.com>
2023-01-28 21:13:51 +0530

    can change access to constructors as wellc

--- src/Constructors/ConstructorsUse.java
+++ src/Constructors/ConstructorsUse.java
@@ -2,10 +2,11 @@ package Constructors;
 
 public class ConstructorsUse {
     public static void main(String[] args) {
-        Constructors c1 = new Constructors(1,2,3);
-        Constructors c2 = new Constructors(1,2);
-        c1.display();
-        c2.display();
+//        Constructors c1 = new Constructors(1,2,3);
+//        Constructors c2 = new Constructors(1,2);
+        Constructors c3 = new Constructors();
+//        c1.display();
+//        c2.display();
     }
 
 
