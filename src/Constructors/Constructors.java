dd18617cd96d7c6a683d2f00418aa1c980345a0b
Jitulteron7 <jitulteron9@gmail.com>
2023-01-28 21:08:26 +0530

    constructors

---
+++ src/Constructors/Constructors.java
@@ -0,0 +1,22 @@
+package Constructors;
+
+public class Constructors {
+    int num1;
+    int num2;
+    int num3;
+
+    Constructors(int num1,int num2,int num3){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    Constructors(int num1,int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+     void display(){
+        System.out.println(num1+" "+num2+" "+num3+" ");
+    }
+}
---
+++ src/Constructors/ConstructorsUse.java
@@ -0,0 +1,13 @@
+package Constructors;
+
+public class ConstructorsUse {
+    public static void main(String[] args) {
+        Constructors c1 = new Constructors(1,2,3);
+        Constructors c2 = new Constructors(1,2);
+        c1.display();
+        c2.display();
+    }
+
+
+
+}
